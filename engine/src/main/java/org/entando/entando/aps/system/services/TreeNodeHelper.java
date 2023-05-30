e8a36d6d6e900bcece09ea18c68b550952bb4366
Luigi Sportelli <l.sportelli@entando.com>
2023-05-30 18:20:51 +0200

    ENG-4886 added impl for absolute position field logs

--- engine/src/main/java/org/entando/entando/aps/system/services/TreeNodeHelper.java
+++ engine/src/main/java/org/entando/entando/aps/system/services/TreeNodeHelper.java
@@ -1,9 +1,14 @@
 package org.entando.entando.aps.system.services;
 
 import com.agiletec.aps.system.common.tree.ITreeNode;
+import com.agiletec.aps.system.common.tree.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+import org.entando.entando.ent.util.EntLogging;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public abstract class TreeNodeHelper<T extends ITreeNode> {
 
     public List<T> getNodes(String parentNodeCode) {
@@ -15,11 +20,15 @@ public abstract class TreeNodeHelper<T extends ITreeNode> {
     }
 
     private List<T> buildNodesList(T parentNode, List<T> nodes, boolean root) {
+        log.debug("build node list for parentNode:'{}'", parentNode.getCode());
         if (root || !this.isNodeAllowed(parentNode)) {
             int absolutePosition = 0;
             for (String childNodeCode : parentNode.getChildrenCodes()) {
                 absolutePosition++;
                 T childNode = this.getTreeNode(childNodeCode);
+                log.debug("iteration build child:'{}' for parentNode:'{}' position:'{}' absolutePosition:'{}'",
+                        childNodeCode, parentNode.getCode(), childNode.getPosition(), absolutePosition);
+
                 if (this.isNodeAllowed(childNode)) {
                     childNode.setAbsolutePosition(absolutePosition);
                     nodes.add(childNode);
