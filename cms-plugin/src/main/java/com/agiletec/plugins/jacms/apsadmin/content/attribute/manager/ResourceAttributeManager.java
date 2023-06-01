692bb05a22082c745219021ea985eac3be9f58d3
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2023-06-01 22:47:07 +0200

    ENG-4896: Fix smell codes and test coverage improvement

--- cms-plugin/src/main/java/com/agiletec/plugins/jacms/apsadmin/content/attribute/manager/ResourceAttributeManager.java
+++ cms-plugin/src/main/java/com/agiletec/plugins/jacms/apsadmin/content/attribute/manager/ResourceAttributeManager.java
@@ -40,9 +40,6 @@ public class ResourceAttributeManager extends TextAttributeManager {
         super.updateAttribute(attribute, tracer, request);
         IResourceManager resourceManager = (IResourceManager) ApsWebApplicationUtils.getBean(JacmsSystemConstants.RESOURCE_MANAGER, request);
         AbstractResourceAttribute resourceAttribute = (AbstractResourceAttribute) attribute;
-        if (null != resourceAttribute.getMetadatas()) {
-            resourceAttribute.getMetadatas().clear();
-        }
         Map<String, List<String>> mapping = resourceManager.getMetadataMapping();
         if (null == mapping || mapping.isEmpty()) {
             return;
@@ -53,7 +50,10 @@ public class ResourceAttributeManager extends TextAttributeManager {
             String formFieldPrefix = tracer.getFormFieldName(attribute) + "_metadata_";
             mapping.keySet().stream().forEach(key -> {
                 String value = request.getParameter(formFieldPrefix + key);
-                if (!StringUtils.isBlank(value)) {
+                if (null != value) {
+                    if (value.trim().length() == 0) {
+                        value = null;
+                    }
                     resourceAttribute.setMetadata(key, currentLang.getCode(), value);
                 }
             });
