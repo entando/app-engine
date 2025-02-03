6a6b24ba37a02c9bf73c8a0d91c38ecd45b154e6
alecasent <a.caschili@entando.com>
2025-02-03 17:46:32 +0100

    Add Virtual Context (in progress... shared last version)

--- engine/src/main/java/com/agiletec/aps/util/ApsTenantApplicationUtils.java
+++ engine/src/main/java/com/agiletec/aps/util/ApsTenantApplicationUtils.java
@@ -44,8 +44,9 @@ public final class ApsTenantApplicationUtils {
 		} else {
 			logger.debug("the custom header:'{}' is empty or blank, skip it", UrlUtils.ENTANDO_TENANT_CODE_CUSTOM_HEADER);
 			String domain = getDomainFromRequest(request);
-			String contextPath = request.getContextPath();
-			String context = contextPath.startsWith("/") ? contextPath.replaceFirst("/", "") : contextPath;
+			//String contextPath = request.getContextPath();
+			//String context = contextPath.startsWith("/") ? contextPath.replaceFirst("/", "") : contextPath;
+			String context = request.getParameter("virtual-context");
 			System.out.println(" *** TenantApplicationUtils *** CONTEXT: `" + context + "`");
 			ITenantManager tenantManager = ApsWebApplicationUtils.getBean(ITenantManager.class, request);
 			String tenantCodeFromDomainAndContext = tenantManager.getTenantCodeByDomainAndContext(domain, context);
